d141a731e2d24f6fa30523e492c70486c6a4c0c0
Mathias <mwip86@gmail.com>
2022-11-18 22:45:58 +0100

    neu

---
+++ passwort_gen.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+const char alphanum[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int string_length = sizeof(alphanum)-1;
+int main()
+{
+    int n;
+    cout<<"Enter the length of password:";
+    cin>>n;
+    srand(time(0));
+    cout<<"Generated password:\n";
+    for(int i = 0; i < n; i++)
+    cout << alphanum[rand() % string_length];
+    cout << "\n";
+    system("Pause");
+    return 0;
+}
