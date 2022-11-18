c9f3ec2e9e15ed435a3f66aef18529c8446cd1c8
Mathias <mwip86@gmail.com>
2022-11-18 20:08:44 +0100

    geäändert

--- Programm wurde gestartet.cpp
+++ Programm wurde gestartet.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-using namespace std;
+
 
 
 int main() {
     std::cout << "Das Programm wurde gestartet!! Arsch\n";
+    system("Pause");
+
+     std::cout << "NICHTS DRÜCKEN\n";
     system("Pause");
     return 0;
 }
