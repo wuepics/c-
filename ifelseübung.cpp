d44f0b9897c3fd776c788b7507eac3e940cfb4dd
Mathias <mwip86@gmail.com>
2022-11-19 17:28:59 +0100

    h

--- ifelseübung.cpp
+++ ifelseübung.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 using namespace std;
 
+/* Funktioniert
 int main(){
     int wert=0;
     std::cout<<"Bitte Zahl eingeben:";
@@ -15,6 +16,32 @@ int main(){
             std::cout <<"Die Zahl " <<"* " <<wert <<" *" <<" wurde erkannt, danke!\n";
             }
     std::cout <<"Das wars auch schon, bis dann und liebe Gruesse\n";
+    system ("Pause");
+std::cout <<"wuepics@git_19.11.22\n";
+system ("Pause");
+}
+*/
+
+// Ab hier Test mit Namensabfrage
+int main(){
+    int wert=0;
+char text[100];
+    std::cout << "Bitte gib Deinen Namen ein: " << endl;
+    std::cin >> text;
+    std::cout << "Hallo " << text << endl;
+
+system ("Pause");
+
+    std::cout<< text << ", bitte eine Zahl eingeben:";
+    std::cin>>wert;
+    if(std::cin.fail()){
+        //Hier Code wenn keine Zahl eingeben wurde
+        std::cout <<"Du sollst eine Zahl eingeben, was los bei dir?\n";
+        }
+        else {
+            std::cout <<"Die Zahl " <<"* " <<wert <<" *" <<" wurde erkannt, danke!\n";
+            }
+    std::cout <<"Das wars auch schon " << text <<", bis dann und liebe Gruesse\n";
     system ("Pause");
 std::cout <<"wuepics@git_19.11.22\n";
 system ("Pause");
